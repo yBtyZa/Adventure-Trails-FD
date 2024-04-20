d97f883dbd0f878fc72462df9b83fc5aed4334d8
Guilherme Betsa <ybtyza@gmail.com>
2024-04-20 15:51:10 -0300

    fix: 🐛 Correção footer

--- src/components/molecules/header/Header.jsx
+++ src/components/molecules/header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <Link to="/"><h4>Adveture Trails FD</h4></Link>
         <div className="link_header">
           <Link to="/trilhas">Explorar Trilhas</Link>
-          <Link to="/cadastro-trilhas">Cadastrar trilhas</Link>
+          <Link to="/cadastro-trilhas">Cadastrar Trilhas</Link>
         </div>
       </div>
     </div>
