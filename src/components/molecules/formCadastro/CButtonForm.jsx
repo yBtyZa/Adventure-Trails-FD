9f3f3479b6224f915fad7d371db492ee1752133f
Guilherme Betsa <ybtyza@gmail.com>
2024-04-20 15:42:38 -0300

    fix: 🐛 Ajuste de botão

--- src/components/molecules/formCadastro/CButtonForm.jsx
+++ src/components/molecules/formCadastro/CButtonForm.jsx
@@ -1,18 +1,19 @@
 import React, { forwardRef } from "react";
 import Button from "../../atoms/button/Button";
+import { Link } from "react-router-dom";
 
 const CButtonForm = forwardRef((props, ref) => {
  return (
   <>
    <Button titulo="Cadastrar" type="submit" ref={ref} />
-   <Button
+   <Link to={"/"}><Button
     style={{
      backgroundColor: "#50490800",
      color: "#504908",
      border: "2px solid #504908"
     }}
     titulo="Voltar"
-   />
+   /></Link>
   </>
  );
 });
