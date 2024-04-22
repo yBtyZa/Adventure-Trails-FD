4a9b6c8a2affe9c000ec600ededbdfeb95e8d2bc
Guilherme Betsa <ybtyza@gmail.com>
2024-04-22 20:51:24 -0300

    feat: 🎸 add imgs

--- src/components/atoms/footer/Footer.jsx
+++ src/components/atoms/footer/Footer.jsx
@@ -1,4 +1,7 @@
 import './Footer.css'
+import logoInstagram from '../../../assets/img/logoInstagram.png'
+import logoLinkedin from '../../../assets/img/logoLinkedin.png'
+import logoGithub from '../../../assets/img/logoGithub.png'
 
 function Footer() {
  return (
@@ -6,15 +9,15 @@ function Footer() {
    <p>Adventure Trails FD - <strong>by: Guilherme Betsa</strong></p>
    <div className='footer_img'>
     <a href="https://github.com/yBtyZa" target='_blank'><img
-     src="https://s3-alpha-sig.figma.com/img/0f5e/e618/a1c0bc5c90eea474d8f54c99e0b2d23f?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SP8JZfAZzwWVjZrgalJAiOOi56LXw7j5GcFeByqFMbmEv1Qxv-m7Xj1tlv~xyemNxUm19bZeXMsj8yydWigK0qPL-sMo~k80bw~2Imu6haJ23cIm-ExMdV3et4NCZxqDsOL-pUSbzryctAEghtQH6CvVbLSO75T~8oxLlPgEEENY6wGWGsHPDXvBMcMAU~ADtvFFxGxZ3~hw8tPbtquzY3kxGHhv-LZ0BykW3AY6ArgbFo6a4HNmGlFcAn1AQRd1YH1KGzmvdKS4wEH3Trjoi-WVB-tcWHBdqdI-sV8vzdPr6QP4pkpC5nQuW2kL~YzR08XTPqdcUiSOOio29Jhzcw__"
+     src={logoGithub}
      alt="github-logo-24"
     /></a>
     <a href="https://www.linkedin.com/in/gbetsa" target='_blank'><img
-     src="https://s3-alpha-sig.figma.com/img/9dc7/c97f/63638c8d890006aa316aca0e31059204?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pjB~XvPav2MO2lL~Va4gpkvLyZPX9RiP2ugqKK196tbG8nv6JBZjrDEYxyWZ960sSlJ5yVY1DpbZuOoGXiY14bMHkdN5nJWHw2uX3AcHfGSMPn6jWlNt33dvH7u-tV7drbUX4BImdoWuvSvrr4GiPfgThRKmhiKYBwPFmoOZQKigloM1-LS6W1ZE-U7brOVNvC9oy1q4glPSBx2JGrIhkVP4dhbuy1MH5Be8Te7yv35cK4tYh4QBidsa~GgSJ8LSklE3J-KLHrhalDE6S-i9hDvGSoqZUkNsEGBojiF6vB0vXhsw~2jduHCju2ypXdj2TfL2BFVrwcdBJKIXhQnXDg__"
+     src={logoLinkedin}
      alt="linkedin-logo-24"
     /></a>
     <a href="https://www.instagram.com/_gbetsa/"><img
-     src="https://s3-alpha-sig.figma.com/img/6506/f731/0244b19eaf76de30f1d029e152e078c9?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Yd1v~jfQDsFmjNoxeaqzRjwkpuLgYkg6sa-ZCPKUquACIHI26yfz0ntGlDTLPoPiW6q35dXxm4n4KEFqUe6iEDjV-JrXuPAadCpfXxy~mBPQwliUtjJnnIcXVFZNfEf5tcZgcwEc6zrsvcOolQJVtGz6dCExUeiRmXVGnPALJPAzLs083-iqr5dpA3Yx-0mynvuBMwU5sa~X1ScuweGxaBiXOYeRwWzj6TGURLw~kXZ8MlZLZnIFUkvvtoSDquMQgf8ashq3J2sm~ox4nsELi8rLhlTsjj9OuKpduJiPxz16qkcN8bHA-TS4Y3MgOMknzISSKPS134uoOn1izuo6sQ__"
+     src={logoInstagram}
      alt="instagram-logo-24"
     /></a>
    </div>
--- src/components/molecules/header/ImagemHeader/ImagemHeader.jsx
+++ src/components/molecules/header/ImagemHeader/ImagemHeader.jsx
@@ -1,7 +1,9 @@
+import imageHeader from "../../../../assets/img/imageHeader.png";
+
 function ImagemHeader({ style }){
     return(
         <img
-        src="https://s3-alpha-sig.figma.com/img/d499/89ee/a6d23a7a9ffd6d136e430c855a72ebc0?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aNCm8Il7udCywQ3z3GwSwkSOXMpYVXyInNfBnh5qT-NQ-nY2VgJnOALnsFzAd9uFW4hgc-b5YLbsf1ffI5-NC2amZdwGcCeUpn-xLvdlY5K4AEDxjH45Tx1B~n0s0GW3PMo9-QFhvEgXqeazAo3gXDxUGXu9~GBLHWypGzvKz2D-bzyvjVTi8sOP6-Ip9Qo3~xsgV5AKYQ~EV9BJ~ZgRYrh~pAMggLyTGAmgdvlknJih80NKqpBrfE10ruSfZ~dFuXtCFtkl1qPkDLkGb3qLFZYT7FFJO9uEa9tTB4T3YBeG6y3laL6zx7MiedtT~LV6unSoeklRBQvxqUfSG8vAjg__"
+        src={ imageHeader }
         alt="imagem-trilha"
         style={style}
       />
